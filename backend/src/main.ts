c83a8bd54d1a082a6241d702baac0ed22ac6feb3
juhanse <julien.hanse12@gmail.com>
2025-09-13 16:19:37 +0200

    add: dotenv package

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+
+dotenv.config({ path: join(__dirname, '../../.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
