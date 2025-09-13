0e9b6891cccedf6623a194ed9cf609856739048a
juhanse <julien.hanse12@gmail.com>
2025-09-13 16:44:23 +0200

    add: config cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,13 @@ import { join } from 'path';
 dotenv.config({ path: join(__dirname, '../../.env') });
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+
+	app.enableCors({
+		origin: 'http://localhost:3001',
+		credentials: true,
+	});
+
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
