6eefc9c73f8b8f61f0b2cd9480194aae4ba016a3
juhanse <julien.hanse12@gmail.com>
2025-09-13 18:40:22 +0200

    add: auth register & login router

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
+export type AuthLogin = {
+	email: string;
+	password: string;
+};
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+	constructor(private readonly authService: AuthService, private readonly usersService: UsersService) {}
+
+	@Post("login")
+	async login(@Body() body: AuthLogin) {
+		return await this.authService.login({ authlogin: body });
+	}
+
+	@Post("register")
+	async register(@Body() body: CreateUserDto) {
+		return await this.authService.register({ authregister: body });
+	}
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Pool } from 'pg';
+import { AuthLogin } from './auth.controller';
+import { compare, hash } from 'bcrypt';
+import { UserPayload } from './jwt.strategy';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(@Inject('PG_POOL') private pool: Pool, private readonly jwtService: JwtService) {}
+
+	async login({ authlogin }: { authlogin: AuthLogin }) {
+		const { email, password } = authlogin;
+
+		const query = 'SELECT id, username, email FROM users WHERE id = $1';
+		const result = await this.pool.query(query, [email]);
+
+		if (!result.rows[0]) {
+			throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+		}
+
+		const isPasswordValid = await this.isPasswordValid({ password, hashedPassword: result.rows[0].password });
+		if (!isPasswordValid) {
+			throw new HttpException('Invalid password', HttpStatus.UNAUTHORIZED);
+		}
+
+		return this.authenticateUser({ id: result.rows[0].id });
+	}
+
+	async register({ authregister }: { authregister: CreateUserDto }) {
+		const { username, email, password } = authregister;
+
+		const searchQuery = 'SELECT id, username, email FROM users WHERE username = $1 OR email = $2';
+		const existingUser = await this.pool.query(searchQuery, [username, email]);
+
+		if (existingUser.rows[0]) {
+			throw new HttpException('User existing', HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		const hashedPassword = await this.hashPassword({ password });
+
+		const createQuery = `
+			INSERT INTO users (username, email, password)
+			VALUES ($1, $2, $3)
+			RETURNING id, username, email
+		`;
+		const user = await this.pool.query(createQuery, [username, email, hashedPassword]);
+
+		if (!user.rows[0]) {
+			throw new HttpException('Failed creating user', HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return this.authenticateUser({ id: user.rows[0].id });
+	}
+
+	private async hashPassword({ password }: { password: string }) {
+		const hashPassword = await hash(password, 10);
+	
+		return hashPassword;
+	}
+
+	private async isPasswordValid({ password, hashedPassword }: { password: string, hashedPassword: string }) {
+		const isPasswordValid = await compare(password, hashedPassword);
+	
+		return isPasswordValid;
+	}
+
+	private authenticateUser({ id }: UserPayload) {
+		const payload: UserPayload = { id };
+	
+		return { access_token: this.jwtService.sign(payload) }
+	}
+}
