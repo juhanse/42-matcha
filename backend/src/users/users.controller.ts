f56265993373a107aa02dc73d6d9d0bb7f7d74ff
juhanse <julien.hanse12@gmail.com>
2025-09-14 16:02:58 +0200

    add: users routing

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Body, Get, Request, Patch, Delete } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UsersService } from './users.service';
+import type { RequestUser } from 'src/auth/jwt.strategy';
+import { UpdateUserDto } from './dto/update-user.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+	constructor(private readonly usersService: UsersService) {}
+
+	@Get('me')
+	getMe(@Request() req: RequestUser) {
+		return this.usersService.getUser({ id: req.user.id });
+	}
+
+	@Patch('me')
+	updateMe(@Request() req: RequestUser, @Body() updateUserDto: UpdateUserDto) {
+		return this.usersService.updateUser({ id: req.user.id } , updateUserDto);
+	}
+
+	@Delete('me')
+	deleteMe(@Request() req: RequestUser) {
+		return this.usersService.deleteUser({ id: req.user.id });
+	}
+}
