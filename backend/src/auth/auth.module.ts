b10d4d77372c3f4f3cbea0f066eda8d7a4460ccf
juhanse <julien.hanse12@gmail.com>
2025-09-14 16:43:17 +0200

    fix: jwt link

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DbModule } from 'src/db/db.module';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt/dist/jwt.module';
+import { config } from 'dotenv';
+import { join } from 'path';
+
+config({ path: join(__dirname, '../../../.env') });
 
 @Module({
+	imports: [
+		DbModule,
+		JwtModule.register({
+			global: true,
+			secret: process.env.JWT_SECRET,
+			signOptions: { expiresIn: '365d' },
+		}),
+	],
 	controllers: [AuthController],
-	providers: [AuthService]
+	providers: [JwtStrategy, AuthService]
 })
 export class AuthModule {}
